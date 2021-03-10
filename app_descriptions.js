02b14d94d94496f27bf6d2394db3358ce9625594
Alex Gurganus <gurgalex@iastate.edu>
2021-03-10 15:59:17 -0600

    Read game data from file

---
+++ app_descriptions.js
@@ -0,0 +1,59 @@
+let asset_path = "./assets/";
+const gamesData = [{
+        title: 'Grim RPG',
+        description: 'A unique "Everything is an Item" incremental - even the options are items that must be found or crafted!',
+        logo: "./assets/Grim_RPG_128_orig.png",
+        playStoreLink: "https://play.google.com/store/apps/details?id=com.topcog.grim.rpg",
+    },
+    {
+        title: 'Simple RPG',
+        description: 'Embark on the epic quest of good VS. evil in this refreshingly simple RPG Idle Clicker Incremental!',
+        logo: "./assets/SRPG_128.png",
+        playStoreLink: "https://play.google.com/store/apps/details?id=com.topcog.simplerpg",
+        appleStoreLink: "https://apps.apple.com/us/app/simple-rpg-idle-tap-tapper/id1489959334",
+    },
+    {
+        title: 'Tap Wizard',
+        description: 'Incremental idle clicker meets classic mage and magic casting.',
+        logo: "./assets/tw-128_orig.png",
+        playStoreLink: "https://play.google.com/store/apps/details?id=com.topcog.tapwizardrpgarcanequest",
+        appleStoreLink: "https://apps.apple.com/us/app/tap-wizard-rpg-arcane-quest/id1369751082",
+    },
+    {
+        title: 'Ninja Prime',
+        description: 'Incremental idle clicker meets classic mage and magic casting.',
+        logo: "./assets/inp-128_orig.png",
+        playStoreLink: "https://play.google.com/store/apps/details?id=com.topcog.idleninjaprime",
+        appleStoreLink: "https://apps.apple.com/us/app/ninja-prime-tap-quest/id1471002445",
+
+    },
+    {
+        title: 'Idle Loot Quest',
+        description: 'Idle, v: to pass time doing nothing. Loot, n: spoils taken by pillaging. Quest, n: a pursuit made in order to obtain something. Idle Loot Quest, n: An epic pursuit to obtain spoils by passing time doing nothing!',
+        logo: "./assets/ilq-128_orig.png",
+        playStoreLink: "https://play.google.com/store/apps/details?id=com.topcog.idlelootquest",
+        appleStoreLink: "https://itunes.apple.com/us/app/idle-loot-quest/id1073634515?mt=8",
+
+    },
+    {
+        title: 'Idle Brain Quest',
+        description: 'Do you have what it takes to be a Master Idling Genius? Can you solve the dilemma of mismatched socks? Sit back and relax as you idle your way through Brain Quests. But be warned! Difficult challenges await, and there is no guarantee of success...',
+        logo: "./assets/ibq-128_orig.png",
+        playStoreLink: "https://play.google.com/store/apps/details?id=com.topcog.idlegenius.android",
+        appleStoreLink: "https://itunes.apple.com/us/app/idle-quest/id949933004?mt=8",
+
+    },
+    {
+        title: 'Idle Quest',
+        description: "Questing! It's what we love, and it's what we do. Begin by getting quests from townsfolk. Then, scurry to the designated questing location with the footgear of your choice. Once there, slay the needed monsters with a flip of your sword, and return to town. Charm the townsfolk with your magical amulet and collect massive piles of gold! Use your newfound loot to craft new gear, letting you tackle with ease the next challenging quest...",
+        logo: "./assets/iq-128_orig.png",
+        playStoreLink: "https://play.google.com/store/apps/details?id=com.topcog.idlequest.android",
+        appleStoreLink: "https://itunes.apple.com/us/app/idle-quest/id949933004?mt=8",
+
+    },
+];
+
+export default gamesData;
+export {
+    gamesData
+};
--- index.jsx
+++ index.jsx
@@ -4,68 +4,23 @@ import GameList from "./GameList";
 import Game from "./Game";
 import ReactDOM from "react-dom";
 import Footer from "./Footer";
+import {gamesData} from "./app_descriptions.js";
 
 const Home = () => {
     return (
         <>
             <Header />
             <GameList>
-                <Game
-                    title='Grim RPG'
-                    description='A unique "Everything is an Item" incremental - even the options are items that must be found or crafted!'
-                    logo="./assets/Grim_RPG_128_orig.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.grim.rpg"
-                />
+                    {gamesData.map(game => {
+                            return <Game
+                                title={game.title}
+                                logo={game.logo}
+                                description={game.description}
+                                appleStoreLink={game.appleStoreLink}
+                                playStoreLink={game.playStoreLink}
+                            />
+                        })}
 
-                <Game
-                    title='Simple RPG'
-                    description='Embark on the epic quest of good VS. evil in this refreshingly simple RPG Idle Clicker Incremental!'
-                    logo="./assets/SRPG_128.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.simplerpg"
-                    appleStoreLink="https://apps.apple.com/us/app/simple-rpg-idle-tap-tapper/id1489959334"
-                />
-
-                <Game
-                    title='Tap Wizard'
-                    description='Incremental idle clicker meets classic mage and magic casting.'
-                    logo="./assets/tw-128_orig.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.tapwizardrpgarcanequest"
-                    appleStoreLink="https://apps.apple.com/us/app/tap-wizard-rpg-arcane-quest/id1369751082"
-                />
-
-                <Game
-                    title='Ninja Prime'
-                    description='Incremental idle clicker meets classic mage and magic casting.'
-                    logo="./assets/inp-128_orig.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idleninjaprime"
-                    appleStoreLink="https://apps.apple.com/us/app/ninja-prime-tap-quest/id1471002445"
-                />
-
-
-                <Game
-                    title='Idle Loot Quest'
-                    description='Idle, v: to pass time doing nothing. Loot, n: spoils taken by pillaging. Quest, n: a pursuit made in order to obtain something. Idle Loot Quest, n: An epic pursuit to obtain spoils by passing time doing nothing!'
-                    logo="./assets/ilq-128_orig.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idlelootquest"
-                    appleStoreLink="https://itunes.apple.com/us/app/idle-loot-quest/id1073634515?mt=8"
-                />
-
-
-                <Game
-                    title='Idle Brain Quest'
-                    description='Do you have what it takes to be a Master Idling Genius? Can you solve the dilemma of mismatched socks? Sit back and relax as you idle your way through Brain Quests. But be warned! Difficult challenges await, and there is no guarantee of success...'
-                    logo="./assets/ibq-128_orig.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idlegenius.android"
-                    appleStoreLink="https://itunes.apple.com/us/app/idle-quest/id949933004?mt=8"
-                />
-
-                <Game
-                    title='Idle Quest'
-                    description="Questing! It's what we love, and it's what we do. Begin by getting quests from townsfolk. Then, scurry to the designated questing location with the footgear of your choice. Once there, slay the needed monsters with a flip of your sword, and return to town. Charm the townsfolk with your magical amulet and collect massive piles of gold! Use your newfound loot to craft new gear, letting you tackle with ease the next challenging quest..."
-                    logo="./assets/iq-128_orig.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idlequest.android"
-                    appleStoreLink="https://itunes.apple.com/us/app/idle-quest/id949933004?mt=8"
-                />
 
             </GameList>
 
