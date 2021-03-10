cf9138ba4b3b88f38c344c7eca5ee2306276deb0
Alex Gurganus <gurgalex@iastate.edu>
2021-03-09 22:37:41 -0600

    Add other games

--- index.jsx
+++ index.jsx
@@ -21,9 +21,52 @@ const Home = () => {
                     title='Simple RPG'
                     description='Embark on the epic quest of good VS. evil in this refreshingly simple RPG Idle Clicker Incremental!'
                     logo="./assets/SRPG_128.png"
-                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.simplerpg&hl=en_US&gl=US"
+                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.simplerpg"
                     appleStoreLink="https://apps.apple.com/us/app/simple-rpg-idle-tap-tapper/id1489959334"
                 />
+
+                <Game
+                    title='Tap Wizard'
+                    description='Incremental idle clicker meets classic mage and magic casting.'
+                    logo="./assets/tw-128_orig.png"
+                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.tapwizardrpgarcanequest"
+                    appleStoreLink="https://apps.apple.com/us/app/tap-wizard-rpg-arcane-quest/id1369751082"
+                />
+
+                <Game
+                    title='Ninja Prime'
+                    description='Incremental idle clicker meets classic mage and magic casting.'
+                    logo="./assets/inp-128_orig.png"
+                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idleninjaprime"
+                    appleStoreLink="https://apps.apple.com/us/app/ninja-prime-tap-quest/id1471002445"
+                />
+
+
+                <Game
+                    title='Idle Loot Quest'
+                    description='Idle, v: to pass time doing nothing. Loot, n: spoils taken by pillaging. Quest, n: a pursuit made in order to obtain something. Idle Loot Quest, n: An epic pursuit to obtain spoils by passing time doing nothing!'
+                    logo="./assets/ilq-128_orig.png"
+                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idlelootquest"
+                    appleStoreLink="https://itunes.apple.com/us/app/idle-loot-quest/id1073634515?mt=8"
+                />
+
+
+                <Game
+                    title='Idle Brain Quest'
+                    description='Do you have what it takes to be a Master Idling Genius? Can you solve the dilemma of mismatched socks? Sit back and relax as you idle your way through Brain Quests. But be warned! Difficult challenges await, and there is no guarantee of success...'
+                    logo="./assets/ibq-128_orig.png"
+                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idlegenius.android"
+                    appleStoreLink="https://itunes.apple.com/us/app/idle-quest/id949933004?mt=8"
+                />
+
+                <Game
+                    title='Idle Quest'
+                    description="Questing! It's what we love, and it's what we do. Begin by getting quests from townsfolk. Then, scurry to the designated questing location with the footgear of your choice. Once there, slay the needed monsters with a flip of your sword, and return to town. Charm the townsfolk with your magical amulet and collect massive piles of gold! Use your newfound loot to craft new gear, letting you tackle with ease the next challenging quest..."
+                    logo="./assets/iq-128_orig.png"
+                    playStoreLink="https://play.google.com/store/apps/details?id=com.topcog.idlequest.android"
+                    appleStoreLink="https://itunes.apple.com/us/app/idle-quest/id949933004?mt=8"
+                />
+
             </GameList>
 
             <Footer />
